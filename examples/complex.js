140f7badca495525fcdaa7988fead267f50975ee
elliottcable <me@ell.io>
2010-01-10 02:45:24 -0900

    Using the new `from.package()` support

--- examples/complex.js
+++ examples/complex.js
@@ -1,4 +1,4 @@
-from.absolute('/Users/elliottcable/Code/poopy.js/lib/poopy.js').wait();
+from.package('poopy.js').wait();
 
 var yarn = from.relative('../lib/Yarn.js').wait(),
      sys = require("sys");
